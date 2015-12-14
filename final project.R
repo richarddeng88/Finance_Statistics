975ba9ddb799cd58a66417a27f586c2bdd7c4a08
Richard <richarddeng88@gmail.com>
2015-12-14 17:53:06 -0500

    12 14 afternoon

--- final project.R
+++ final project.R
@@ -1,23 +1,40 @@
-
 library(quantmod)
-stocklist = c('PPG',  'STZ',  'AIG',  'LB'   ,'AXP'  ,'RRC'  ,'ORCL', 'AVB',  'NTRS', 
-              'HIG',  'MAS','STX','KMI','BHI','FFIV', 'PRU',  'WEC'  ,'MAC',"MKC","CMI")
+library(copula)   # for copula functions
+library(fGarch)   # need for standardized t density
+library(MASS)     # need for fitdistr and kde2d
+library(fCopulae) # additional copula functions (pempiricalCopula
+# and ellipticalCopulaFit)
+library(quantmod)
+library(xts)
+library(timeDate)
 
-par(mfrow=c(4,5))
-for (i in stocklist) {
+####  choose 20 stocks from S^P 500
+    SPcompany = read.table("data/finance_stats/SP500 companies.csv", header=T, sep=",")
+    colnames(SPcompany)
+    table(SPcompany[,"GICSector"])  
+    stocklist = c('AMZN',  'COH',  'EXPE',  'MCD'   ,'NKE'  ,'RL'  ,'CCE', 'PEP',  'AXP', 
+                  'AIG',  'BLK','COF','GS','JPM','MCO', 'WFC',  'FDX'  ,'MMM','AAPL','FB')
+# consumer discretionary industry:  amazon, coach, expedia, 
+# consumer staples industry :       McDonald's, NIKE, Ralph Lauren, coca-cola, Pepsico.inc, 
+# financial industry:               American express, AIG, black stone, capital one, goldman sachs group, JPmogan, moody's, wells fargo, 
+# industrials:                      Fedex,3M,
+# information technology industry : apple, facebook. 
+
+### DATA PREPROCESSING
+    par(mfrow=c(4,5))
+    for (i in stocklist) {
+        
+        x = get(getSymbols(i, src = 'yahoo', from = '2014-12-04', adjust=TRUE))
+        xr = dailyReturn(x, type = "log")
+        plot(xr, main=i)
+        #plot(x[,4],main=i)
+        assign(i, xr)
+    }
     
-    x = get(getSymbols(i, src = 'yahoo', from = '2014-12-04', adjust=TRUE))
-    xr = dailyReturn(x, type = "log")
-    #plot(xr, main=i)
-    #plot(x[,4],main=i)
-    assign(i, xr)
-    
-}
-
-
-dat <- cbind(PPG, STZ,	AIG,LB,	AXP,RRC,ORCL,AVB,NTRS,	HIG,MAS,STX,KMI,BHI,FFIV,PRU,WEC,MAC,MKC,CMI)
-dat <- as.data.frame(dat)
-names(dat) <- stocklist 
+    dat <- cbind(AMZN,  COH,  EXPE, MCD ,NKE ,RL ,CCE, PEP, AXP, 
+                 AIG, BLK,COF,GS,JPM,MCO, WFC, FDX ,MMM,AAPL,FB)
+    dat <- as.data.frame(dat)
+    names(dat) <- stocklist 
 
         # heatmap
         library(lattice)
@@ -40,46 +57,55 @@ names(dat) <- stocklist
         correlation <- cor(dat)
         myheatmap(correlation,-1,1,F)
 
-
-library(MASS)
-est_mean <- apply(dat,2, function(x){as.numeric(fitdistr(x,"normal")$estimate)[1]})
-est_sd <-  apply(dat,2, function(x){as.numeric(fitdistr(x,"normal")$estimate)[2]})
-
-
-# tranfore the log daily return to fited normal marginal distribution
-data1 <- matrix(0,dim(dat)[1],20)
-for (i in 1:20){
-    data1[,i] <- pnorm(dat[,i], mean=est_mean[i], sd=est_sd[i])
-}
-range(as.vector(data1))
+        # QQ - plot
+        for (i in 1:20){
+            qqnorm(dat[,2])
+        }
+        
+ 
+# DATA TRANSFORMATION       
+        library(MASS)
+        est_mean <- apply(dat,2, function(x){as.numeric(fitdistr(x,"normal")$estimate)[1]})
+        est_sd <-  apply(dat,2, function(x){as.numeric(fitdistr(x,"normal")$estimate)[2]})
+        
+        
+        # tranfore the log daily return to fited normal marginal distribution
+        data1 <- matrix(0,dim(dat)[1],20)
+        for (i in 1:20){
+            data1[,i] <- pnorm(dat[,i], mean=est_mean[i], sd=est_sd[i])
+        }
+            
+            # show transformed histgram compared to the original one
+            par(mfrow=c(1,2))
+            #hist(dat[,1], main="original daily returns",breaks=15,xlab='PPG')
+            hist(data1[,1],main="Normal transformed returns",breaks=15,xlab='PPG')
+            hist(data1[,2],main="Normal transformed returns",breaks=15,xlab='STZ')
+            
+            # transfored data histgram
+            for (i in 1:20)
+            {
+                hist(dat[,i], xlab="ori data",main = stocklist[i])
+            }
+            
     
-    # show transformed histgram compared to the original one
-    par(mfrow=c(1,2))
-    #hist(dat[,1], main="original daily returns",breaks=15,xlab='PPG')
-    hist(data1[,1],main="Normal transformed returns",breaks=15,xlab='PPG')
-    hist(data1[,2],main="Normal transformed returns",breaks=15,xlab='STZ')
 
-################################ placeholder ######################
-library(copula)   # for copula functions
 
-### Fit normalCopula
-fnorm = fitCopula(data=data1[,1:20],  
-                  method = "itau", optim.method="BFGS", 
-                  copula=normalCopula(dim=20, dispstr="un"))
+### MODEL BUILDING -  Fit normalCopula
+        fnorm = fitCopula(data=data1[,1:20],  
+                          method = "itau", optim.method="BFGS", 
+                          copula=normalCopula(dim=20, dispstr="un"))
 
         ### Estimating df parameter for t-copula
-        cor_tau = cor(data1, method="kendall")
-        t_omega = sin(pi*cor_tau/2)
-        cop_t_dim2 = tCopula(t_omega, dim = 20, dispstr = "un", df = 190)
+        #cor_tau = cor(data1, method="kendall")
+        #t_omega = sin(pi*cor_tau/2)
+        #cop_t_dim2 = tCopula(t_omega, dim = 20, dispstr = "un", df = 190)
         
-        ft2 = fitCopula(cop_t_dim2, data=data1,
-                        method = "ml", optim.method="L-BFGS-B", 
-                        start=c(t_omega,5),lower=c(0,2.5),upper=c(.5,15) )
+        #ft2 = fitCopula(cop_t_dim2, data=data1,
+         #               method = "ml", optim.method="L-BFGS-B", 
+          #              start=c(t_omega,5),lower=c(0,2.5),upper=c(.5,15) )
         
-        ncopula <- normalCopula(dim=20, param = c())
+        #ncopula <- normalCopula(dim=20, param = c())
 
-########## Best Copula ###############
-bestcopula <- fnorm
 
 ########## VaR ########################
 mean1 <- apply(dat,2, mean)
@@ -112,12 +138,18 @@ list <-   list(list(mean= mean1[1],sd=sd1[1]),
 
 mvdc_norm <- mvdc(copula = normalCopula(coef(fnorm),dim=20,dispstr="un"), rep("norm", 20), list)
 
-    # simulate 10000 daily returns
+## SIMULATING NEW DATA SET :  simulate 10000 daily returns
     set.seed(2015)
     number=10000
     rand_mvdc <- rMvdc(n=number, mvdc=mvdc_norm)
-    #pairs(rand_mvdc)
+        #pairs(rand_mvdc)
+        # simulated data histgram
+        for (i in 1:20)
+        {
+            hist(rand_mvdc[,i], main = stocklist[i])
+        }
     
+# CALCULATION : 
     # Based on the simulation data, we make a portfolio with each stock equally weighted
     meanRet <- apply(rand_mvdc, 1, mean)
     x <- meanRet[order(meanRet)]
@@ -126,7 +158,8 @@ mvdc_norm <- mvdc(copula = normalCopula(coef(fnorm),dim=20,dispstr="un"), rep("n
     VaR5perc.port <- -invest*x[number*0.05]
     ES5per.port <- -invest*(mean(x[1:number*0.05]))
 
-## assume our portofolio has 20 stocks and they are Equally Weighted
+
+    ## assume our portofolio has 20 stocks and they are Equally Weighted
     # calculate mean and sd 
     meanRet <- mean(apply(dat, 2, mean))
     sdRet <- sqrt(sum(apply(dat, 2, sd)^2)/400)
